cfcc4ac5d0a486f4ea3a0e603962184b27c8a2f2
mannyoii <mannyoii@protonmail.com>
2023-04-30 20:24:30 +0530

    Add solutions for javascript-exercises 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,8 @@
 const removeFromArray = function (arr, ...num) {
   for (let i = 0; i < num.length; i++) {
-    for (let j = 0; i < arr.length; j++) {
-      if (num[i] === arr[j]) {
-        arr.splice(i, 1);
-      }
+    const index = arr.indexOf(num[i]);
+    if (index > -1) {
+      arr.splice(index, 1);
     }
   }
   return arr;
