d6440ee2e7b9a94ebd0fb510a16511b676a4b58d
mannyoii <mannyoii@protonmail.com>
2023-04-30 21:07:50 +0530

    Add solutions for javascript-exercises 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function (firstNum, secondNum) {
+  // if (firstNum < 0 || secondNum || 0) return "ERROR";
+  if (typeof firstNum === "number" && typeof secondNum === "number") {
+    if (firstNum >= 0 && secondNum >= 0) {
+      return firstNum < secondNum
+        ? [secondNum * (firstNum + secondNum)] / 2
+        : [firstNum * (firstNum + secondNum)] / 2;
+    } else return "ERROR";
+  } else return "ERROR";
 };
 
 // Do not edit below this line
